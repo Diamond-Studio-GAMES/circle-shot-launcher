e2e225b5cfc9a2f971342719fa41869bcfd07024
Summersay415 <summersay415@gmail.com>
2024-11-22 20:17:54 +0700

    Немного переименовал

--- launcher/launcher.gd
+++ launcher/launcher.gd
@@ -56,16 +56,22 @@ func list_local_versions() -> void:
 		child.queue_free()
 	
 	var version_nodes: Array[Node]
-	for version: String in versions_file.get_sections():
+	for version_code: String in versions_file.get_sections():
 		var version_node: PanelContainer = _version_scene.instantiate()
-		version_node.name = StringName(version)
-		var version_name: String = "Версия %s" % _get_version_name(version)
-		if versions_file.get_value(version, "beta"):
+		version_node.name = StringName(version_code)
+		var version_name: String = "Версия %s" % _get_version_name(version_code)
+		if versions_file.get_value(version_code, "beta"):
 			version_name += " (БЕТА)"
 		(version_node.get_node(^"%VersionName") as Label).text = version_name
-		(version_node.get_node(^"%Run") as Button).pressed.connect(run_version.bind(version))
-		(version_node.get_node(^"%Export") as Button).pressed.connect(export_version.bind(version))
-		(version_node.get_node(^"%Remove") as Button).pressed.connect(remove_version.bind(version))
+		(version_node.get_node(^"%Run") as Button).pressed.connect(
+				run_version.bind(version_code)
+		)
+		(version_node.get_node(^"%Export") as Button).pressed.connect(
+				export_version.bind(version_code)
+		)
+		(version_node.get_node(^"%Remove") as Button).pressed.connect(
+				remove_version.bind(version_code)
+		)
 		version_nodes.append(version_node)
 	
 	version_nodes.sort_custom(func(first: Node, second: Node) -> bool:
@@ -83,20 +89,20 @@ func list_local_versions() -> void:
 	save_files()
 
 
-func run_version(version: String) -> void:
-	_status.text = "Запускаю версию %s..." % _get_version_name(version)
-	print("Starting version %s..." % _get_version_name(version))
+func run_version(version_code: String) -> void:
+	_status.text = "Запускаю версию %s..." % _get_version_name(version_code)
+	print("Starting version %s..." % _get_version_name(version_code))
 	
-	if not _is_version_files_valid(version):
+	if not _is_version_files_valid(version_code):
 		_validate_version_configs()
-		push_error("Run: can't find files of version %s." % _get_version_name(version))
+		push_error("Run: can't find files of version %s." % _get_version_name(version_code))
 		_status.text = "Запуск: файлы версии не найдены. Удаляю эту версию."
 		_reset_status_timer.start()
-		_remove_version(version)
+		_remove_version(version_code)
 		return
 	
-	var executable_path: String = _get_version_engine_path(version)
-	var pack_path: String = _get_version_pack_path(version)
+	var executable_path: String = _get_version_engine_path(version_code)
+	var pack_path: String = _get_version_pack_path(version_code)
 	var pid: int = OS.create_process(executable_path, PackedStringArray(["--main-pack", pack_path]))
 	if pid == -1:
 		push_error("Can't start game with executable path %s and pack path %s!" % [
@@ -110,18 +116,18 @@ func run_version(version: String) -> void:
 		get_tree().quit.call_deferred()
 
 
-func export_version(version: String) -> void:
-	if not _is_version_files_valid(version):
-		push_error("Export: can't find files of version %s." % _get_version_name(version))
+func export_version(version_code: String) -> void:
+	if not _is_version_files_valid(version_code):
+		push_error("Export: can't find files of version %s." % _get_version_name(version_code))
 		_status.text = "Экспорт: файлы версии не найдены. Удаляю эту версию."
 		_reset_status_timer.start()
-		_remove_version(version)
+		_remove_version(version_code)
 		return
 	
 	var efd: FileDialog = $ExportFileDialog
-	efd.title = "Экспорт версии %s" % _get_version_name(version)
-	efd.current_file = "game_v%s.zip" % _get_version_name(version)
-	efd.file_selected.connect(_export_version.bind(version))
+	efd.title = "Экспорт версии %s" % _get_version_name(version_code)
+	efd.current_file = "game_v%s.zip" % _get_version_name(version_code)
+	efd.file_selected.connect(_export_version.bind(version_code))
 	efd.popup_centered()
 	efd.visibility_changed.connect(
 			efd.file_selected.disconnect.bind(_export_version),
@@ -129,10 +135,10 @@ func export_version(version: String) -> void:
 	)
 
 
-func remove_version(version: String) -> void:
+func remove_version(version_code: String) -> void:
 	var dd: ConfirmationDialog = $DeleteDialog
-	dd.dialog_text = "Удалить версию %s?" % _get_version_name(version)
-	dd.confirmed.connect(_remove_version.bind(version))
+	dd.dialog_text = "Удалить версию %s?" % _get_version_name(version_code)
+	dd.confirmed.connect(_remove_version.bind(version_code))
 	dd.popup_centered()
 	dd.visibility_changed.connect(
 			dd.confirmed.disconnect.bind(_remove_version),
@@ -140,12 +146,12 @@ func remove_version(version: String) -> void:
 	)
 
 
-func _export_version(path: String, version: String) -> void:
-	if not _is_version_files_valid(version):
-		push_error("Export: can't find files of version %s." % _get_version_name(version))
+func _export_version(path: String, version_code: String) -> void:
+	if not _is_version_files_valid(version_code):
+		push_error("Export: can't find files of version %s." % _get_version_name(version_code))
 		_status.text = "Экспорт: файлы версии не найдены. Удаляю эту версию."
 		_reset_status_timer.start()
-		_remove_version(version)
+		_remove_version(version_code)
 		return
 	
 	print("Exporting to %s." % path)
@@ -164,24 +170,24 @@ func _export_version(path: String, version: String) -> void:
 		return
 	
 	var config := ConfigFile.new()
-	config.set_value("config", "name", _get_version_name(version))
-	config.set_value("config", "code", version)
+	config.set_value("config", "name", _get_version_name(version_code))
+	config.set_value("config", "code", version_code)
 	config.set_value("config", "engine_version", \
-			versions_file.get_value(version, "engine_version"))
+			versions_file.get_value(version_code, "engine_version"))
 	config.set_value("config", "arch", Engine.get_architecture_name())
 	config.set_value("config", "platform", OS.get_name())
-	config.set_value("config", "beta", versions_file.get_value(version, "beta"))
+	config.set_value("config", "beta", versions_file.get_value(version_code, "beta"))
 	
 	zip.start_file("version.cfg")
 	zip.write_file(config.encode_to_text().to_utf8_buffer())
 	zip.close_file()
 	
 	zip.start_file("engine")
-	zip.write_file(FileAccess.get_file_as_bytes(_get_version_engine_path(version)))
+	zip.write_file(FileAccess.get_file_as_bytes(_get_version_engine_path(version_code)))
 	zip.close_file()
 	
-	zip.start_file("data")
-	zip.write_file(FileAccess.get_file_as_bytes(_get_version_pack_path(version)))
+	zip.start_file("pack")
+	zip.write_file(FileAccess.get_file_as_bytes(_get_version_pack_path(version_code)))
 	zip.close_file()
 	
 	zip.close()
@@ -209,11 +215,11 @@ func _import_version(path: String) -> void:
 	
 	if not (
 			zip.file_exists("engine")
-			and zip.file_exists("data")
+			and zip.file_exists("pack")
 			and zip.file_exists("version.cfg")
 	):
-		_status.text = "Этот ZIP-файл не является версией игры."
-		push_error("ZIP file is missing some of files (engine, data, versions.cfg)")
+		_status.text = "В этом ZIP-файле нет нужных файлов."
+		push_error("ZIP file is missing some files (engine, pack, versions.cfg)")
 		_reset_status_timer.start()
 		($MouseBlock as CanvasItem).hide()
 		return
@@ -242,12 +248,12 @@ func _import_version(path: String) -> void:
 		($MouseBlock as CanvasItem).hide()
 		return
 	
-	var version_code: String = config.get_value("config", "code")
+	var new_version_code: String = config.get_value("config", "code")
 	var engine_version: String = config.get_value("config", "engine_version")
 	
-	if version_code in versions_file.get_sections():
-		_status.text = "Эта версия (%s) уже установлена!" % _get_version_name(version_code)
-		push_error("This version (%s) is already installed." % _get_version_name(version_code))
+	if new_version_code in versions_file.get_sections():
+		_status.text = "Эта версия (%s) уже установлена!" % _get_version_name(new_version_code)
+		push_error("This version (%s) is already installed." % _get_version_name(new_version_code))
 		_reset_status_timer.start()
 		($MouseBlock as CanvasItem).hide()
 		return
@@ -260,8 +266,8 @@ func _import_version(path: String) -> void:
 		return
 	
 	var engine_already_installed := false
-	for version: String in versions_file.get_sections():
-		if versions_file.get_value(version, "engine_version", "-1.0") == engine_version:
+	for version_code: String in versions_file.get_sections():
+		if versions_file.get_value(version_code, "engine_version", "-1.0") == engine_version:
 			engine_already_installed = true
 			break
 	
@@ -274,17 +280,17 @@ func _import_version(path: String) -> void:
 		return
 	
 	# Всё наконец-то ОК, распаковываем!
-	var pack_file := FileAccess.open(_get_version_pack_path(version_code), FileAccess.WRITE)
+	var pack_file := FileAccess.open(_get_version_pack_path(new_version_code), FileAccess.WRITE)
 	if not pack_file:
 		_status.text = "Ошибка распаковки файла с ресурсами!"
 		push_error("Error creating pack file at path %s. Error: %s" % [
-			_get_version_pack_path(version_code),
+			_get_version_pack_path(new_version_code),
 			error_string(FileAccess.get_open_error())
 		])
 		_reset_status_timer.start()
 		($MouseBlock as CanvasItem).hide()
 		return
-	pack_file.store_buffer(zip.read_file("data"))
+	pack_file.store_buffer(zip.read_file("pack"))
 	pack_file.close()
 	
 	if not engine_already_installed:
@@ -307,69 +313,71 @@ func _import_version(path: String) -> void:
 			# Выдаём разрешения на запуск
 			OS.execute("/bin/chmod", PackedStringArray(["+x", engine_file.get_path_absolute()]))
 	
-	versions_file.set_value(version_code, "name", config.get_value("config", "name"))
-	versions_file.set_value(version_code, "beta", config.get_value("config", "beta"))
-	versions_file.set_value(version_code, "engine_version", engine_version)
+	versions_file.set_value(new_version_code, "name", config.get_value("config", "name"))
+	versions_file.set_value(new_version_code, "beta", config.get_value("config", "beta"))
+	versions_file.set_value(new_version_code, "engine_version", engine_version)
 	save_files()
 	list_local_versions()
-	_status.text = "Импорт версии %s завершён." % _get_version_name(version_code)
+	_status.text = "Импорт версии %s завершён." % _get_version_name(new_version_code)
 	_reset_status_timer.start()
 	($MouseBlock as CanvasItem).hide()
-	print("Import of version %s ended." % _get_version_name(version_code))
+	print("Import of version %s ended." % _get_version_name(new_version_code))
 
 
-func _remove_version(version: String) -> void:
-	DirAccess.remove_absolute(_get_version_pack_path(version))
+func _remove_version(version_code: String) -> void:
+	DirAccess.remove_absolute(_get_version_pack_path(version_code))
 	
 	var delete_engine := true
 	for section: String in versions_file.get_sections():
-		if version == section:
+		if version_code == section:
 			continue
-		if versions_file.get_value(version, "engine_version", "-1.0") == \
+		if versions_file.get_value(version_code, "engine_version", "-1.0") == \
 				versions_file.get_value(section, "engine_version", "-2.0"):
 			delete_engine = false
 			break
 	
 	if delete_engine:
-		DirAccess.remove_absolute(_get_version_engine_path(version))
+		DirAccess.remove_absolute(_get_version_engine_path(version_code))
 	
-	versions_file.erase_section(version)
-	print("Removed version %s." % version)
+	var version_name: String = _get_version_name(version_code)
+	versions_file.erase_section(version_code)
+	print("Removed version %s." % version_code)
 	list_local_versions()
+	_status.text = "Версия %s удалена." % version_name
 
 
 func _validate_version_configs() -> void:
-	for version: String in versions_file.get_sections():
+	for version_code: String in versions_file.get_sections():
 		if not (
-				version.is_valid_int()
-				and versions_file.has_section_key(version, "name")
-				and versions_file.has_section_key(version, "engine_version")
-				and versions_file.has_section_key(version, "beta")
-				and _is_version_files_valid(version)
+				version_code.is_valid_int()
+				and versions_file.has_section_key(version_code, "name")
+				and versions_file.has_section_key(version_code, "engine_version")
+				and versions_file.has_section_key(version_code, "beta")
+				and _is_version_files_valid(version_code)
 		):
-			push_error("Found incorrect version config: %s." % _get_version_name(version))
-			_remove_version(version)
+			push_error("Found incorrect version config: %s." % _get_version_name(version_code))
+			_remove_version(version_code)
 	
 	save_files()
 
 
-func _get_version_name(version: String) -> String:
-	return versions_file.get_value(version, "name", version)
+func _get_version_name(version_code: String) -> String:
+	return versions_file.get_value(version_code, "name", version_code)
 
 
-func _is_version_files_valid(version: String) -> bool:
-	return FileAccess.file_exists(_get_version_engine_path(version)) \
-			and FileAccess.file_exists(_get_version_pack_path(version))
-
-
-func _get_version_engine_path(version: String, engine_version := "") -> String:
+func _get_version_engine_path(version_code: String, engine_version := "") -> String:
 	if engine_version.is_empty():
 		return data_path.path_join(
-				"engine." + str(versions_file.get_value(version, "engine_version"))
+				"engine." + str(versions_file.get_value(version_code, "engine_version"))
 				+ executable_suffix
 		)
 	return data_path.path_join("engine." + engine_version + executable_suffix)
 
 
-func _get_version_pack_path(version: String) -> String:
-	return data_path.path_join("data." + version + ".pck")
+func _get_version_pack_path(version_code: String) -> String:
+	return data_path.path_join("pack." + version_code + ".pck")
+
+
+func _is_version_files_valid(version_code: String) -> bool:
+	return FileAccess.file_exists(_get_version_engine_path(version_code)) \
+			and FileAccess.file_exists(_get_version_pack_path(version_code))
